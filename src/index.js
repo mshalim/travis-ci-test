20907e5e99e74a6913f228713a61f6d7e42f8956
Mario Halim <mario.s.halim@gmail.com>
2020-05-31 11:03:55 -0700

    first push

---
+++ src/index.js
@@ -0,0 +1,9 @@
+class Index {
+  static test() {
+    return 'this is a test';
+  }
+}
+
+module.exports = {
+  Index: Index
+}
---
+++ src/index.spec.js
@@ -0,0 +1,7 @@
+const Index = require('./index');
+
+describe('index', () => {
+  it('should test something', () => {
+    expect(Index.Index.test()).toEqual('this is a test');
+  })
+})
